b04b269fc634eb4eebfad4b7e21caf7f2d7fe305
Lucas <74400487+lucasdns@users.noreply.github.com>
2023-11-23 12:31:12 +0100

    Add files via upload
    mise à jours avec 02_hello_vpc

--- main.tf
+++ main.tf
@@ -2,8 +2,22 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0aafdae616ee7c9b7"
-  instance_type = "t2.micro"
+resource "aws_vpc" "my_vpc" {
+ cidr_block = "10.0.0.0/16"
+ enable_dns_support = true
+ enable_dns_hostnames = true
+
+ tags = {
+   Name = "MonVPC"
+ }
 }
 
+resource "aws_subnet" "my_subnet" {
+ vpc_id                  = aws_vpc.my_vpc.id
+ cidr_block              = "10.0.1.0/24"
+ map_public_ip_on_launch = true
+
+ tags = {
+   Name = "MonRéseau"
+ }
+}
